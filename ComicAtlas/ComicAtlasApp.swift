e7b8e910e9b3d50ad4352b067e927c9ce4a203cd
Bogdan Semeniuk <000sbv000@gmail.com>
2026-03-02 13:02:48 +0200

    Initial commit

---
+++ ComicAtlas/ComicAtlasApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComicAtlasApp.swift
+//  ComicAtlas
+//
+//  Created by Богдан Семенюк on 02.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct ComicAtlasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
